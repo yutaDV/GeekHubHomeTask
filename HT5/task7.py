a58bc2622585c7175bafa5697752c634e1e45797
yutaDV <yutadv@gmail.com>
2022-10-20 22:49:47 +0300

    corht5.7

--- HT5/task7.py
+++ HT5/task7.py
@@ -8,7 +8,8 @@
 
 def copies(test_list):
 
-    newlist = [x if x is not True else 'True' for x in test_list]
+    newlist_0 = [x if x is not True else 'True' for x in test_list]
+    newlist = [x if x is not False else 'False' for x in newlist_0]
     oreginal_list = []
     for element in newlist:
         if element not in oreginal_list:
@@ -17,6 +18,8 @@ def copies(test_list):
     for element in oreginal_list:
         if element is True:
             result += f'{True} -> {newlist.count(element)}, '
+        if element is False:
+            result += f'{False} -> {newlist.count(element)}, '
         else:
             result += f'{element} -> {newlist.count(element)}, '
     return f'{result}'
@@ -24,6 +27,6 @@ def copies(test_list):
 
 test_list = [
     False, 1, 1, 'foo', [1, 2], 'foo', 56, 55, -111,
-    True, {5: 2}, 'foo', [1, 2], True, {5: 2}, -111
+    True, {5: 2}, 'foo', [1, 2], True, {5: 2}, -111, 0
 ]
 print(copies(test_list))
