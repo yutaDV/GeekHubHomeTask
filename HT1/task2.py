3e0c4fc16f6758feadd1f18b380c1a37f0b7ba61
yutaDV <yutadv@gmail.com>
2022-10-25 23:57:19 +0300

    ht7

--- HT1/task2.py
+++ HT1/task2.py
@@ -1,1 +1,2 @@
-print ('осноні оперції git робота з git')
+for i in range(1, 100, 0):
+	print(i)
---
+++ HT7/task3.py
@@ -0,0 +1,25 @@
+'''НT7.3.    ви знаєте таку функцію як <range>. Напишіть свою реалізацію
+цієї функції. Тобто щоб її можна було використати у вигляді:
+    for i in my_range(1, 10, 2):
+        print(i)
+    1
+    3
+    5
+    7
+    9
+P.S. Повинен вертатись генератор.
+P.P.S. Для повного розуміння цієї функції - можна почитати документацію по ній:
+ https://docs.python.org/3/library/stdtypes.html#range
+P.P.P.S Не забудьте обробляти невалідні ситуації (типу range(1, -10, 5) тощо).
+ Подивіться як веде себе стандартний range в таких випадках..'''
+return
+
+def my_range(start=0, stop, step=1):
+
+    if step == 0:
+        return 
+
+    return(min(words))
+
+
+print(count_string('kjhsajkhgd agjhjfd afjhdg'))
