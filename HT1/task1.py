c339b205d8fd8e043f4b7760fc1e9eca44ada4bf
yutaDV <yutadv@gmail.com>
2022-10-31 01:12:50 +0200

    ht8.3

--- HT1/task1.py
+++ HT1/task1.py
@@ -5,4 +5,16 @@ print("File one __name__ is set to: {}" .format(__name__))
 if __name__ == "__main__":
    print("File one executed when ran directly")
 else:
-   print("File one executed when imported") 
+   print("File one executed when imported")
+
+   with open('users.csv', 'w', newline='') as file:
+    writer = csv.DictWriter(file, fieldnames=['user_name','password'])
+    users_data = [
+        {'user_name':'hare', 'password': 'carrot54'},
+        {'user_name': 'fox', 'password': 'hare4567'},
+        {'user_name': 'wolf', 'password': 'fox44fox'},
+        {'user_name':'bear', 'password': 'honey1256l'}
+    ]
+    writer.writeheader()
+    writer.writerows(users_data)
+
--- HT8/task 3/task3.py
+++ HT8/task 3/task3.py
@@ -15,14 +15,15 @@
          додається в кінець файла;
       - файл з користувачами: тільки читається. Але якщо захочете
           реалізувати функціонал додавання нового користувача
-      - не стримуйте себе :)
+          - не стримуйте себе :)
    Особливості функціонала:
       - за кожен функціонал відповідає окрема функція;
       - основна функція - <start()> - буде в собі містити весь
          workflow банкомата:
       - на початку роботи - логін користувача (програма запитує ім'я/пароль).
         Якщо вони неправильні - вивести повідомлення про це і закінчити роботу
-       (хочете - зробіть 3 спроби, а потім вже закінчити роботу - все на ентузіазмі :))
+       (хочете - зробіть 3 спроби, а потім вже закінчити роботу
+             - все на ентузіазмі :))
       - потім - елементарне меню типн:
         Введіть дію:
            1. Подивитись баланс
@@ -30,34 +31,142 @@
            3. Вихід
       - далі - фантазія і креатив, можете розширювати функціонал,
        але основне завдання має бути повністю реалізоване :)
-    P.S. Увага! Файли мають бути саме вказаних форматів (csv, txt, json відповідно)
+    P.S. Увага! Файли мають бути саме вказаних форматів
+         (csv, txt, json відповідно)
     P.S.S. Добре продумайте структуру програми та функцій'''
 
+import csv
+import json
+import datetime
 
-def open_file(name, amount):
-    '''The function takes a file and returns 3 parts of it'''
 
-    try:
-        f = open(name)
-    except FileNotFoundError:
-        return 'Dont found the file'
-    else:
-        f.close()
-    with open(name) as file:
-        result = []
-        len_fale = len(file.read())
-        if len_fale == 0:
-            return 'The file is emty'
-        if len_fale < amount:
-            return f'Invalid arguments, amount cannot be begger than {len_fale}'
-        file.seek(0)
-        result.append(file.read(amount))
-        file.seek(len_fale // 2)
-        result.append(file.read(amount))
-        file.seek(len_fale - amount)
-        result.append(file.read(amount))
+def verification(username, user_password):
+    ''' the function checks the password'''
+
+    with open('users.csv', newline='') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            if row['user_name'] == username and row['password'] == user_password:
+                return True
+        return False
+
+
+def greeting():
+    '''the function requests user data returns verified username'''
+
+    attempt = 0
+    while attempt < 3:
+        username = input('Please, enter your user name:  ')
+        user_password = input('Please, enter your password:  ')
+        if verification(username, user_password):
+            return username
+        else:
+            print("Oops!!! Username or password is incorrect. Try again... ")
+            attempt += 1
+    print('The entered data is incorrect, contact the bank')
+    return None
+
+
+def menu():
+    ''' choice of action, the fuction returns user_action'''
+    menu = {
+        'balance': 'Look at the balance',
+        'top_up': 'Top up the balance',
+        'get': 'Receiving money',
+        'exit': 'Exit'
+    }
+    while True:
+        print('Please, choice of action')
+        for key, value in menu.items():
+            print(f'{key} for {value}')
+        user_action = input('Enter one of the actions listed above:')
+        if user_action in menu:
+            return user_action
+        else:
+            print('Oops!!! the action is incorrect. Try again...')
+
+
+def balance(username):
+    '''The function looks at the balance'''
+
+    with open(f'{username}_balance.txt') as file:
+        result = file.read()
+    to_json = {
+        'time': f'{datetime.datetime.now()}',
+        'action': 'Look at the balance',
+        'balance': result
+    }
+    with open(f'json.{username}_transactions.JSON', 'a') as file:
+        json.dump(to_json, file)
+        return f'Your belance:{result}'
+
+
+def top_up(username):
+    '''The function tops up  the user balance'''
+
+    while True:
+        try:
+            user_sum = float(input('Enter the top-up amount: '))
+            break
+        except:
+            print("Oops!!! The amount is incorrect. Try again... ")
+    file_name = f'{username}_balance.txt'
+    with open(file_name) as file:
+        user_balance = float(file.read())
+    result = user_balance + user_sum
+    with open(file_name, 'w') as file:
+        file.write(f'{result}')
+    to_json = {
+        'time': f'{datetime.datetime.now()}',
+        'action': 'Top up the balance',
+        'top_up_sum': user_sum, 'finaly_balance': result
+    }
+    with open(f'json.{username}_transactions.JSON', 'a') as file:
+        json.dump(to_json, file)
         return result
 
 
+def receiving(username):
+    '''The function receis money  the user balance'''
+
+    with open(f'{username}_balance.txt') as file:
+        user_balance = float(file.read())
+    while True:
+        try:
+            user_sum = float(input('What amount of money you want to receive: '))
+        except:
+            print("Oops!!! The amount is incorrect. Try again... ")
+        else:
+            if user_sum <= user_balance:
+                break
+            else:
+                print("Oops!!! You do not have that amount of money. Try again... ")
+    result = user_balance - user_sum
+    with open(f'{username}_balance.txt', 'w') as file:
+        file.write(f'{result}')
+    to_json = {
+        'time': f'{datetime.datetime.now()}',
+        'action': 'Receiving money',
+        'top_up_sum': user_sum, 'finaly_balance': result
+    }
+    with open(f'json.{username}_transactions.JSON', 'a') as file:
+        json.dump(to_json, file)
+        return result
+
+
+def start():
+    '''workflow of ATM'''
+
+    username = greeting()
+    if username:
+        user_action = menu()
+    else:
+        return "The end"
+    if user_action == 'exit':
+        return "Good bye"
+    else:
+        return "Good bye"
+
+
 if __name__ == "__main__":
-    print(open_file('doc_ht8_2.txt', 10))
+    print(balance('fox'))
