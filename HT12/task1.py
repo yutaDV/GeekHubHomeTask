5d31fa5a916097171d8b97b9a31d3bf3f4ed8d3b
yutaDV <yutadv@gmail.com>
2022-11-20 01:39:48 +0200

    ht12start

--- HT12/task1.py
+++ HT12/task1.py
@@ -4,56 +4,78 @@
 (Square) містять методи _init_ для завдання початкових розмірів
 об'єктів при їх створенні.'''
 
+import math
+
 
 class Figure:
+    """ Батьківський клас для фігур які за замовчуванням є білими
+
+    атрибутами класу є колів фігур який за замовчуванням є білий
+    функція color_change довзволяє зінити колів фігури на вибір користувача"""
     color = 'white'
-    ''' Обєкта
 
-    def color_change():
-        last_result = None
-    """The class performs arithmetic operations with two numbers +, -, /, *
-
-    The class includes four objects, two of which have int or float
-    which are the arguments of mathematical operations, the other two
-    are the default - None that store the current and previous result """
-
-    def add(self, number_1, number_2):
-        """ a simple function adds two numbers"""
-        self.last_result = self.result
-        self.result = number_1 + number_2
-        return self.result
-
-    def subtraction(self, number_1, number_2):
-        """ a simple function subtracts the second number from the first"""
-        self.last_result = self.result
-        self.result = number_1 - number_2
-        return self.result
-
-    def multi(self, number_1, number_2):
-        """a simple function multiplies two numbers"""
-        self.last_result = self.result
-        self.result = number_1 * number_2
-        return self.result
-
-    def division(self, number_1, number_2):
-        """ a simple function performs multiplication and division,
-        in case of division by 0 a warning"""
-        self.last_result = self.result
-        if number_2 == 0:
-            return 'division by 0 is not allowed'
-        else:
-            self.result = number_1 * number_2
-        return self.result
+    def color_change(self):
+        """Функція зміни кольору фігури на вибір користувача"""
+        color_dict = {
+            1: 'black', 2: 'white', 3: 'blue', 4: 'yellow',
+            5: 'green', 6: 'red', 7: 'purple', 8: 'gray'
+        }
+        new_color = None
+        print('If you want to change a color of the figure chose a new color')
+        for key, value in color_dict.items():
+            print(f'Input {key} for {value}')
+        while True:
+            try:
+                new_color = int(input('Input a number of the color: '))
+            except:
+                print('Incorrect data, enter a number from 1 to 8. Try again')
+            else:
+                if 0 < new_color < 9:
+                    break
+                else:
+                    print('Enter a number from 1 to 8. Try again')
+        self.color = color_dict.get(new_color)
+        return
 
 
-set_1 = MathOper()
+class Oval(Figure):
+    """Клас створення овалу за його параметрами"""
 
-print(set_1.last_result)
-set_1.add(1, 1)
-print(set_1.last_result)
-set_1.multi(2, 3)
-print(set_1.last_result)
-set_1.multi(3, 4)
-print(set_1.last_result)
-set_1.multi(3, 4)
-print(set_1.last_result)
+    def __init__(self, big_radius, small_radius):
+        self.big_radius = big_radius
+        self.small_radius = small_radius
+
+    def perimetr(self):
+        big = self.big_radius
+        small = self.small_radius
+        return 4 * ((math.pi * big * small + (big + small) ** 2) / (big + small))
+
+    def area(self):
+        return self.big_radius * self.small_radius * math.pi
+
+
+class Square(Figure):
+    """Клас створення квадрату за його параметрами
+
+    функції класу розраховують периметр та площу квадрату"""
+
+    def __init__(self, side):
+        self.side = side
+
+    def perimetr(self):
+        return self.side * 4
+
+    def area(self):
+        return self.side ** 2
+
+
+set_1 = Square(9)
+print(set_1.color)
+set_2 = Oval(10, 5)
+set_1.color_change()
+print(set_1.color)
+print(set_2.perimetr())
+print(set_2.area())
+print(set_2.color)
+print(set_1.perimetr())
+print(set_1.area())
